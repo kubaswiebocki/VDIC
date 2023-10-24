caa6b81668ee83021aa5293e35d5ff4047203262
kubaswiebocki <kuba.sw@interia.pl>
2023-10-24 22:42:25 +0200

    feat: Add coverage module

--- lab02/tb/vdic_dut_2023_tb.sv
+++ lab02/tb/vdic_dut_2023_tb.sv
@@ -28,7 +28,7 @@ typedef enum bit[2:0] {
     INVALID_A_B  	  = 3'b010,
     VALID_A_INVALID_B = 3'b011,
     VALID_B_INVALID_A = 3'b100,
-    ACK_OP  		  = 3'b101
+    ACK_RDY_OP  	  = 3'b101
 } operation_t;
 
 
@@ -84,6 +84,163 @@ test_result_t       test_result = TEST_PASSED;
 vdic_dut_2023 DUT (.clk, .rst_n, .arg_a, .arg_a_parity, .arg_b, .arg_b_parity, .req, .ack, .result, .result_parity, .result_rdy, .arg_parity_error);
 
 //------------------------------------------------------------------------------
+// Coverage block
+//------------------------------------------------------------------------------
+
+// Covergroup checking the op codes and theri sequences
+covergroup op_cov;
+
+    option.name = "cg_op_cov";
+
+    coverpoint op_set {
+        // #A1 test valid data at the input
+        bins A1_valid_data_AB = VALID_A_B;
+
+        // #A2 test invalid data at the input
+        bins A2_invalid_data_AB = INVALID_A_B;
+
+        // #A3 test invalid data at the A input, valid data at the B input
+        bins A3_invalid_A_valid_B = VALID_B_INVALID_A;
+
+        // #A4 test invalid data at the B input, valid data at the A input
+        bins A4_valid_A_invalid_B = VALID_A_INVALID_B;
+
+        // #A5 single-cycle operation after multiply
+        bins A5_ack_rdy_op = ACK_RDY_OP;
+    }
+
+endgroup
+
+// Covergroup checking for min and max arguments of the ALU
+covergroup corner_values_on_ops;
+
+    option.name = "cg_corner_values_on_ops";
+
+    all_ops : coverpoint op_set {
+        ignore_bins null_ops = {RST_OP, ACK_RDY_OP};
+    }
+
+    a_leg: coverpoint arg_a {
+	    bins min  		= {16'sh8000};
+	    bins minus_one	= {16'shFFFF};
+        bins zeros 		= {16'sh0000};
+        bins others	    = {[16'shFFFE:16'sh8001], [16'sh0002:16'sh7FFE]};
+	    bins one  		= {16'sh0001};
+        bins max  		= {16'sh7FFF};
+    }
+
+    b_leg: coverpoint arg_b {
+	    bins min  		= {16'sh8000};
+	    bins minus_one	= {16'shFFFF};
+        bins zeros 		= {16'sh0000};
+        bins others	    = {[16'shFFFE:16'sh8001], [16'sh0002:16'sh7FFE]};
+	    bins one  		= {16'sh0001};
+        bins max  		= {16'sh7FFF};
+    }
+
+    B_op_corners: cross a_leg, b_leg, all_ops {
+	    
+	    // #B1 Simulate maximum value for signed data.   
+
+        bins B1_vab_max          = binsof (all_ops) intersect {VALID_A_B} &&
+        (binsof (a_leg.max) || binsof (b_leg.max));
+
+        bins B1_iab_max          = binsof (all_ops) intersect {INVALID_A_B} &&
+        (binsof (a_leg.max) || binsof (b_leg.max));
+
+        bins B1_vbia_max         = binsof (all_ops) intersect {VALID_B_INVALID_A} &&
+        (binsof (a_leg.max) || binsof (b_leg.max));
+
+        bins B1_vaib_max         = binsof (all_ops) intersect {VALID_A_INVALID_B} &&
+        (binsof (a_leg.max) || binsof (b_leg.max));
+
+	    // #B2 Simulate minimum value for signed data.   
+
+        bins B2_vab_min          = binsof (all_ops) intersect {VALID_A_B} &&
+        (binsof (a_leg.min) || binsof (b_leg.min));
+
+        bins B2_iab_min          = binsof (all_ops) intersect {INVALID_A_B} &&
+        (binsof (a_leg.min) || binsof (b_leg.min));
+
+        bins B2_vbia_min         = binsof (all_ops) intersect {VALID_B_INVALID_A} &&
+        (binsof (a_leg.min) || binsof (b_leg.min));
+
+        bins B2_vaib_min         = binsof (all_ops) intersect {VALID_A_INVALID_B} &&
+        (binsof (a_leg.min) || binsof (b_leg.min));
+	    
+	    // #B3 Simulate one value for signed data.   
+
+        bins B3_vab_one          = binsof (all_ops) intersect {VALID_A_B} &&
+        (binsof (a_leg.one) || binsof (b_leg.one));
+
+        bins B3_iab_one          = binsof (all_ops) intersect {INVALID_A_B} &&
+        (binsof (a_leg.one) || binsof (b_leg.one));
+
+        bins B3_vbia_one         = binsof (all_ops) intersect {VALID_B_INVALID_A} &&
+        (binsof (a_leg.one) || binsof (b_leg.one));
+
+        bins B3_vaib_one         = binsof (all_ops) intersect {VALID_A_INVALID_B} &&
+        (binsof (a_leg.one) || binsof (b_leg.one));
+
+	    // #B4 Simulate minus one value for signed data.   
+
+        bins B4_vab_mone          = binsof (all_ops) intersect {VALID_A_B} &&
+        (binsof (a_leg.minus_one) || binsof (b_leg.minus_one));
+
+        bins B4_iab_mone          = binsof (all_ops) intersect {INVALID_A_B} &&
+        (binsof (a_leg.minus_one) || binsof (b_leg.minus_one));
+
+        bins B4_vbia_mone         = binsof (all_ops) intersect {VALID_B_INVALID_A} &&
+        (binsof (a_leg.minus_one) || binsof (b_leg.minus_one));
+
+        bins B4_vaib_mone         = binsof (all_ops) intersect {VALID_A_INVALID_B} &&
+        (binsof (a_leg.minus_one) || binsof (b_leg.minus_one));
+	    
+        // #B5 simulate zeros values
+
+        bins B5_vab_0          = binsof (all_ops) intersect {VALID_A_B} &&
+        (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+        bins B5_iab_0          = binsof (all_ops) intersect {INVALID_A_B} &&
+        (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+        bins B5_vbia_0          = binsof (all_ops) intersect {VALID_B_INVALID_A} &&
+        (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+        bins B5_vaib_0          = binsof (all_ops) intersect {VALID_A_INVALID_B} &&
+        (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+        ignore_bins others_only =
+        binsof(a_leg.others) && binsof(b_leg.others);
+    }
+
+endgroup
+
+op_cov                      oc;
+corner_values_on_ops        c_00_FF;
+
+initial begin : coverage
+    oc      = new();
+    c_00_FF = new();
+    forever begin : sample_cov
+        @(posedge clk);
+        if(req || !rst_n) begin
+            oc.sample();
+            c_00_FF.sample();
+            
+            /* #1step delay is necessary before checking for the coverage
+             * as the .sample methods run in parallel threads
+             */
+            #1step; 
+            if($get_coverage() == 100) break; //disable, if needed
+            
+            // you can print the coverage after each sample
+//            $strobe("%0t coverage: %.4g\%",$time, $get_coverage());
+        end
+    end
+end : coverage
+
+//------------------------------------------------------------------------------
 // Clock generator
 //------------------------------------------------------------------------------
 
@@ -125,7 +282,7 @@ function operation_t get_op();
 	    3'b010 : return INVALID_A_B;
         3'b011 : return VALID_A_INVALID_B;
         3'b100 : return VALID_B_INVALID_A;
-	    3'b101 : return ACK_OP;
+	    3'b101 : return ACK_RDY_OP;
 	    3'b110 : return RST_OP;
 	    3'b111 : return RST_OP;
 	    default : return RST_OP;
@@ -281,7 +438,7 @@ initial begin : tester
 	            get_parity(arg_a, 1'b0, arg_a_parity);
 	            get_parity(arg_b, 1'b1, arg_b_parity);
             end : case_vd_b_ivd_a_blk
-            ACK_OP: begin: case_ack_blk
+            ACK_RDY_OP: begin: case_ack_blk
 	            timeout = 0;
 	            req	= 1'b1;
 	            while(!ack && !result_rdy && timeout<1000) begin
