cb6df63e92d0af3377e4e4f3e9abce35d00d52c9
kubaswiebocki <kuba.sw@interia.pl>
2023-10-25 21:09:14 +0200

    fix: MULT tests list correction

--- lab02/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
+++ lab02/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
@@ -1,2 +1,2 @@
 1697697180 /home/student/jswiebocki/VDIC/common/vdic_dut_2023.svp
-1698259406 /home/student/jswiebocki/VDIC/lab02/tb/vdic_dut_2023_tb.sv
+1698259974 /home/student/jswiebocki/VDIC/lab02/tb/vdic_dut_2023_tb.sv
