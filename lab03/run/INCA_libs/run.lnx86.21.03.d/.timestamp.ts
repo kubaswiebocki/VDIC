4756b7c777738f86416fa2c61540b7e74c6ce2a3
kubaswiebocki <kuba.sw@interia.pl>
2023-10-28 22:45:02 +0200

    chore: Update run files in lab03

---
+++ lab03/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
@@ -0,0 +1,7 @@
+1698525342 /home/student/jswiebocki/VDIC/lab03/tb/mult_bfm.sv
+1698524242 /home/student/jswiebocki/VDIC/lab03/tb/scoreboard.sv
+1698307464 /home/student/jswiebocki/VDIC/lab03/tb/mult_pkg.sv
+1698502004 /home/student/jswiebocki/VDIC/lab03/tb/coverage.sv
+1698524793 /home/student/jswiebocki/VDIC/lab03/tb/tpgen.sv
+1697697180 /home/student/jswiebocki/VDIC/common/vdic_dut_2023.svp
+1698525722 /home/student/jswiebocki/VDIC/lab03/tb/top.sv
---
+++ lab03/run/INCA_libs/run.lnx86.21.03.d/worklib_-3.ts
@@ -0,0 +1,2 @@
+1621522767 /eda/cadence/2021-22/RHELx86/XCELIUM_21.03.009/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
+1621522762 /eda/cadence/2021-22/RHELx86/XCELIUM_21.03.009/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
