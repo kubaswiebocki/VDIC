6bcac632b94b040bf0fc5f5d6c9daf1001be9d28
kubaswiebocki <kuba.sw@interia.pl>
2023-10-31 23:24:09 +0100

    fix: build file repair

--- lab03/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
+++ lab03/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
@@ -4,4 +4,4 @@
 1698502004 /home/student/jswiebocki/VDIC/lab03/tb/coverage.sv
 1698524793 /home/student/jswiebocki/VDIC/lab03/tb/tpgen.sv
 1697697180 /home/student/jswiebocki/VDIC/common/vdic_dut_2023.svp
-1698525722 /home/student/jswiebocki/VDIC/lab03/tb/top.sv
+1698525749 /home/student/jswiebocki/VDIC/lab03/tb/top.sv
