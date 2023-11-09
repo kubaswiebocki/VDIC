eff1acb05b0d47085d00583a3a85b6891ebb89f3
kubaswiebocki <kuba.sw@interia.pl>
2023-11-09 08:23:22 +0100

    chore: sim files updated

--- lab03/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
+++ lab03/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
@@ -1,7 +1,6 @@
-1699396377 /home/student/jswiebocki/VDIC/lab03/tb/mult_bfm.sv
+1699398858 /home/student/jswiebocki/VDIC/lab03/tb/mult_bfm.sv
 1699396040 /home/student/jswiebocki/VDIC/lab03/tb/scoreboard.sv
 1698307464 /home/student/jswiebocki/VDIC/lab03/tb/mult_pkg.sv
-1699398381 /home/student/jswiebocki/VDIC/lab03/tb/coverage.sv
 1698794286 /home/student/jswiebocki/VDIC/lab03/tb/tpgen.sv
 1697697180 /home/student/jswiebocki/VDIC/common/vdic_dut_2023.svp
-1699398708 /home/student/jswiebocki/VDIC/lab03/tb/top.sv
+1699513971 /home/student/jswiebocki/VDIC/lab03/tb/top.sv
