1403fe29b0130c5199a4bb046200a3195852505a
kubaswiebocki <kuba.sw@interia.pl>
2023-12-17 17:15:52 +0100

    init: add example lab09 files

---
+++ lab09example/run/INCA_libs/run.lnx86.21.03.d/.timestamp.ts
@@ -0,0 +1,6 @@
+1697697180 /home/student/jswiebocki/VDIC/common/tinyalu/three_cycle.sv
+1697697180 /home/student/jswiebocki/VDIC/common/tinyalu/tinyalu.sv
+1697697180 /home/student/jswiebocki/VDIC/common/tinyalu/single_cycle.sv
+1702314683 /home/student/jswiebocki/VDIC/lab09example/tb/tinyalu_bfm.sv
+1702314683 /home/student/jswiebocki/VDIC/lab09example/tb/tinyalu_pkg.sv
+1702314683 /home/student/jswiebocki/VDIC/lab09example/tb/top.sv
